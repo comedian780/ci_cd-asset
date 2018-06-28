451f5a79db7fadce4e7eaec21529a7bb5fdc12fa
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-06-28 22:33:12 +0200

    Images löschen vor dem Start

--- startImages.sh
+++ startImages.sh
@@ -11,12 +11,18 @@ if (docker ps | grep parcel-asset-option); then
     docker rm -f parcel-asset-option
   fi
 fi
+if (docker images -q parcel-asset-option); then
+  docker rmi -f "${registry_url}:443/parcel-asset-option"
+fi
 
 if (docker ps | grep parcel-asset-address); then
   docker stop parcel-asset-address
   if (docker container ls -al | grep parcel-asset-address); then
     docker rm -f parcel-asset-address
   fi
+fi
+if (docker images -q parcel-asset-address); then
+  docker rmi -f "${registry_url}:443/parcel-asset-address"
 fi
 
 if (docker ps | grep parcel-asset-size); then
@@ -25,12 +31,18 @@ if (docker ps | grep parcel-asset-size); then
     docker rm -f parcel-asset-size
   fi
 fi
+if (docker images -q parcel-asset-size); then
+  docker rmi -f "${registry_url}:443/parcel-asset-size"
+fi
 
 if (docker ps | grep parcel-asset); then
   docker stop parcel-asset
   if (docker container ls -al | grep parcel-asset); then
     docker rm -f parcel-asset
   fi
+fi
+if (docker images -q parcel-asset); then
+  docker rmi -f "${registry_url}:443/parcel-asset"
 fi
 docker run -d --restart always --network=parcelassetnetwork --name=parcel-asset-option "${registry_url}:443/parcel-asset-option"
 docker run -d --restart always --network=parcelassetnetwork --name=parcel-asset-address "${registry_url}:443/parcel-asset-address"
